0f7e74dd0d808b7a896fb8ee7cacbaf02a8d30ce
johnpioc <johnpioc15@gmail.com>
2026-04-24 21:43:47 +1000

    feat: setup backend for terraform state

--- infra/providers.tf
+++ infra/providers.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "jpioc-terraform-state"
+    key            = "personal-website/terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
