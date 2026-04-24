7086b1d0918368bee2061f7c9d81ce1eaf96ba29
johnpioc <johnpioc15@gmail.com>
2026-04-24 21:25:44 +1000

    feat: add site contents to bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,9 @@
+# S3 Bucket for Site Content
 resource "aws_s3_bucket" "site" {
   bucket = "${var.project_name}-site-content-bucket"
 }
 
+# S3 Bucket Policy
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
 
@@ -9,4 +11,22 @@ resource "aws_s3_bucket_public_access_block" "site" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+# Upload Site Contents to S3
+locals {
+  mime_types = {
+    ".html" = "text/html"
+    ".css"  = "text/css"
+  }
+}
+
+resource "aws_s3_object" "site_files" {
+  for_each = fileset("../src", "**/*")
+
+  bucket       = aws_s3_bucket.site.id
+  key          = each.value
+  source       = "../site/${each.value}"
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), "application/octet-stream")
+  etag         = filemd5("../${each.value}")
 }
